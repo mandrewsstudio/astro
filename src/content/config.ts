bf13ecd5a165f2d8133bc4b83264a5a2eee87088
mandrewsstudio <mike@mandrews.studio>
2025-11-25 20:18:43 +0000

    dates and stuff

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,9 +4,6 @@ const home = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    date: z.date(),
-    author: z.string().default('In Progress'),
     buttonText: z.string().optional(),
     buttonLink: z.string().optional(),
     video: z.string().optional(),
